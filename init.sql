c950e728e2dcec7acf1a24db864d67ff182fbf6a
Heetesh Kamthe <heeteshkamthe09@gmail.com>
2025-10-31 13:17:35 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,16 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS facebook;
+
+-- Select the database
+USE facebook;
+
+-- Create 'users' table
+CREATE TABLE IF NOT EXISTS users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    website VARCHAR(100),
+    comment TEXT,
+    gender VARCHAR(10)
+);
+
